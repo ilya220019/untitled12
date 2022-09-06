c1be5732b6f326fd57b9523bc7d87990c2610b57
ilya220019 <112873209+ilya220019@users.noreply.github.com>
2022-09-06 19:57:33 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,50 @@
 import java.util.Random;
-
 public class Main {
-    public static double temperatyraAndAge(double age, double temperatyra) {
-        return age + temperatyra;
+    public static double ageAndTemperature(double age, double temperature) {
+        return age + temperature;
+    }
+    public static int generateRandomAge(int age){
+        return age;
     }
 
     public static void main(String[] args) {
-        double tempAndAge = temperatyraAndAge(23, 48);
+        double tempAndAge = ageAndTemperature(20, 48);
         System.out.println(tempAndAge);
-        int ageOfpeople = 35;
-        int temperature1 = 20;
+        int ageOfPeople = 35;
+        int temperature = 20;
+        if ((ageOfPeople >= 20 && ageOfPeople <= 45) && (temperature >= -20 && temperature <= 30)) {
+            System.out.println("Можно идти гулять");
 
-        if ((ageOfpeople >= 20 && ageOfpeople <= 45) && (temperature1 >= -20 &&
-                temperature1 <= 30)) {
+        }else if (ageOfPeople <=20 && (temperature >=0 && temperature <=28)) {
             System.out.println("Можно идти гулять");
-        } else if ((ageOfpeople < 20) && (temperature1 >= 0 &&
-                temperature1 <= 28)) {
+        }else if (ageOfPeople >=45 && (temperature >=-10 && temperature <=25)) {
             System.out.println("Можно идти гулять");
-        } else if ((ageOfpeople > 45) && (temperature1 >= -10 &&
-                temperature1 <= 25)) {
-            System.out.println("Можно идти гулять");
-        } else {
+        }else {
             System.out.println("Оставайтесь дома");
         }
-
-        double tempAndAge0 = temperatyraAndAge(12, 10);
-        double tempAndAge1 = temperatyraAndAge(9, 8);
-        double tempAndAge2 = temperatyraAndAge(26, 9);
-        double tempAndAge3 = temperatyraAndAge(56, 32);
-        double tempAndAge4 = temperatyraAndAge(28, 34);
-        System.out.println(tempAndAge0 + " " + tempAndAge1 + " " + tempAndAge2 + " " + tempAndAge3 + " " + tempAndAge4);
-
-        g();
-    }
-
-
-    public static int generateRandomAge(int age1) {
-
-
-        return age1;
-    }
-
-    public static void g() {
-        int b = (int) (Math.random() * (90 + 1));
-        generateRandomAge(b);
-        System.out.println(b);
+        double tempAndAge1 = ageAndTemperature(25, -10);
+        double tempAndAge2 = ageAndTemperature(28, 15);
+        double tempAndAge3 = ageAndTemperature(30, 48);
+        double tempAndAge4 = ageAndTemperature(32, 0);
+        double tempAndAge5 = ageAndTemperature(45, 31);
+        System.out.println(tempAndAge1 + " " + tempAndAge2 + " " + tempAndAge3 + " " + tempAndAge4 + " " + tempAndAge5);
+        
         int a = (int) (Math.random() * (90 + 1));
-        int ageOfpeople = generateRandomAge(a);
-        int temperature1 = 20;
+        generateRandomAge(a);
+        System.out.println(a);
+        
+        int ageOfPeople1 = generateRandomAge(a);
+        if ((ageOfPeople1 >= 20 && ageOfPeople1 <= 45) && (temperature >= -20 && temperature <= 30)) {
+            System.out.println("Можно идти гулять");
 
-
-        if ((ageOfpeople >= 20 && ageOfpeople <= 45) && (temperature1 >= -20 &&
-                temperature1 <= 30)) {
+        }else if (ageOfPeople1 <=20 && (temperature >=0 && temperature <=28)) {
             System.out.println("Можно идти гулять");
-        } else if ((ageOfpeople < 20) && (temperature1 >= 0 &&
-                temperature1 <= 28)) {
+        }else if (ageOfPeople1 >=45 && (temperature >=-10 && temperature <=25)) {
             System.out.println("Можно идти гулять");
-        } else if ((ageOfpeople > 45) && (temperature1 >= -10 &&
-                temperature1 <= 25)) {
-            System.out.println("Можно идти гулять");
-        } else {
+        }else {
             System.out.println("Оставайтесь дома");
         }
 
-
     }
+
 }
